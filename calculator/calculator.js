3341ecc1311de7bd0153ef9cfcfa8caf137a6b28
Brent Bartlett <brent.bartlett1@gmail.com>
2015-07-31 17:16:02 -0700

    put old methods back in

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,23 +1,41 @@
 var calculator = {
-    findPrecision: function()
+    findPrecision: function( a, b )
 	{
-        var precision = 0, highestPrecision = 0;
-        
-        // copy arguments into an array
-        var nums = Array.prototype.slice.call( arguments );
-        nums.forEach( function( num ){
-            num = String( num );
-            if( num.indexOf( "." ) !== -1 ){
-                precision = num.split( "." )[ 1 ].length;
-                if ( precision > highestPrecision )
-                    highestPrecision = precision;
-            }
-        } );
-		return highestPrecision;
+        "use strict";
+		var precis = 0, precisA = 0, precisB = 0;
+		
+		// Make sure that they're strings so that indexOf can be used
+		a = String( a );
+		b = String( b );
+		
+		// Find precision of both operands
+		precisA = a.indexOf( "." ) !== -1 ? ( a.length - 1 ) - a.indexOf( "." ) : 0;
+		precisB = b.indexOf( "." ) !== -1 ? ( b.length - 1 ) - b.indexOf( "." ) : 0;
+		
+		// Find longer precision and return it
+		precis = precisA > precisB ? precisA : precisB;
+		return precis;
 	},
-	add: function( a, b )
+    add: function( a, b )
 	{
+        "use strict";
 		var precision = calculator.findPrecision( a, b );
 		return Math.round( ( +a + +b ) * Math.pow( 10, precision ) ) / Math.pow( 10, precision );
+	},
+	subtract: function( a, b )
+	{
+        "use strict";
+		var precision = calculator.findPrecision( a, b );
+		return Math.round( ( +a - +b ) * Math.pow( 10, precision ) ) / Math.pow( 10, precision );
+	},
+	multiply: function( a, b )
+	{
+        "use strict";
+		return Math.round( ( +a * +b ) * Math.pow( 10, 7 ) ) / Math.pow( 10, 7 );
+	},
+	divide: function( a, b )
+	{
+        "use strict";
+		return Math.round( ( +a / +b ) * Math.pow( 10, 7 ) ) / Math.pow( 10, 7 );
 	}
 };
--- calculator/test.js
+++ calculator/test.js
@@ -1,20 +1,31 @@
 var test = {
     results: document.getElementById( "results" ),
     assert: function assert( value, desc ){
+        "use strict";
         var li = document.createElement( "li" );
         li.className = value ? "pass" : "fail";
         li.appendChild( document.createTextNode( desc ) );
         test.results.appendChild( li );
+        if ( !value ){
+			li.parentNode.parentNode.className = "fail";
+		}
         return li;
     },
+    testGroup: function testGroup( name, fn ){
+        "use strict";
+        test.results = document.getElementById( "results" );
+        test.results = test.assert( true, name ).appendChild( document.createElement( "ul" ) );
+		fn();
+    },
     init: function(){
-        test.assert( calculator.findPrecision( 1, 3 ) === 0, "1, 3, greatest precision is 0" );
-        test.assert( calculator.findPrecision( 1, 1.1 ) === 1, "1, 1.1, greatest precision is 1" );
-        test.assert( calculator.findPrecision( 2.6, 5.4 ) === 1, "greatest precision is 1" );
-        test.assert( calculator.findPrecision( .56, 3 ) === 2, "greatest precision is 2" );
-        test.assert( calculator.findPrecision( 0.521455523385445, 3.232323 ) === 15, "greatest precision is 15" );
-        test.assert( calculator.findPrecision( 0.52145, 3, 6.55, .77, 0.0, 2.6, -1.555555 ) === 6, "greatest precision is 6" );
-        
+        "use strict";
+        test.testGroup( function(){
+            test.assert( calculator.findPrecision( 1, 3 ) === 0, "1, 3, greater precision is 0" );
+            test.assert( calculator.findPrecision( 1, 1.1 ) === 1, "1, 1.1, greater precision is 1" );
+            test.assert( calculator.findPrecision( 2.6, 5.4 ) === 1, "greater precision is 1" );
+            test.assert( calculator.findPrecision( .56, 3 ) === 2, "greater precision is 2" );
+            test.assert( calculator.findPrecision( 0.521455523385445, 3.232323 ) === 15, "greater precision is 15" );
+        } );
     }
 }
 test.init();
